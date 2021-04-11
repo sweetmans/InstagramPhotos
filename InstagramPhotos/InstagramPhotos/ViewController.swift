b7cb5f5f51e36c1eca75362d69f8c7a9beccb7aa
ANDY HUANG <ishare1994@icloud.com>
2021-04-11 20:09:12 +0800

    Update readme and other information

--- InstagramPhotos/InstagramPhotos/ViewController.swift
+++ InstagramPhotos/InstagramPhotos/ViewController.swift
@@ -1,20 +1,15 @@
 //
-//  ViewController.swift
-//  Demo
-//
-//  Created by MacBook Pro on 2017/4/21.
-//  Copyright © 2017年 Sweetman, Inc. All rights reserved.
+//  Copyright © 2021年 Sweetman, Inc. All rights reserved.
 //
 
 import UIKit
 import SMInstagramPhotoPicker
 import Photos
 
-class ViewController: UIViewController, SMPhotoPickerViewControllerDelegate {
-    
+class ViewController: UIViewController {
     var picker: SMPhotoPickerViewController?
-    
     @IBOutlet var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         PHPhotoLibrary.requestAuthorization { [weak self] status in
@@ -41,7 +36,6 @@ class ViewController: UIViewController, SMPhotoPickerViewControllerDelegate {
     
     //No things to show here.
     @IBAction func nothings(_ sender: UIButton) {
-        
         let alert = UIAlertController.init(title: "都说啥都没有啦，还点。。。", message: "Do not understan Chinaese? You need google translate.", preferredStyle: .alert)
         let cancel = UIAlertAction.init(title: "我错了😯", style: .cancel, handler: { (action) in
             
@@ -50,25 +44,15 @@ class ViewController: UIViewController, SMPhotoPickerViewControllerDelegate {
         DispatchQueue.main.async {
             self.present(alert, animated: true, completion: nil)
         }
-        
     }
-    
+}
+
+extension ViewController: SMPhotoPickerViewControllerDelegate {
     func didCancelPickingPhoto() {
         print("User cancel picking image")
     }
     
-    
     func didFinishPickingPhoto(image: UIImage, meteData: [String : Any]) {
-        
         imageView.image = image
     }
 }
-
-
-
-
-
-
-
-
-
